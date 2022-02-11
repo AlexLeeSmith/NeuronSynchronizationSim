7835fbdfa94a642bc8a06f80c004114fb9def8fa
Alex Smith <alsmi14@ilstu.edu>
2022-02-11 11:55:52 -0600

    works but ugly, eulers does wierd stuff

--- numerical_methods.c
+++ numerical_methods.c
@@ -7,104 +7,191 @@
 
 /** Preprocessor Directives **/
 #include <stdio.h>      // printf()
-#include <math.h>       // ceil()
+#include <math.h>       // ceil(), exp()
 #include <stdlib.h>     // exit(), EXIT_SUCCESS
+
+#define xR -1.56F
+#define r 0.006F
+#define I 3.1F
+
+/** Global Variables **/
+float s = 1.5;
 
 /** Structures **/
 typedef struct {
-    float *approx;
+    float x0;
+    float xEnd;
+    float *inits;
+    float step;
+} EqConditions;
+
+typedef struct {
+    float **approx;
     float *x;
-    int size;
-} Solution;
+    int funcCount;
+    int stepCount;
+} EqSolution;
 
 /** Forward Declarations **/
-Solution runEulers(float (*)(float, float), float, float, float, float);
-Solution runRungeKutta(float (*)(float, float), float, float, float, float);
+void runEulers(float *(*)(float [], float), EqConditions *, EqSolution *);
+void runRungeKutta(float *(*)(float [], float), EqConditions *, EqSolution *);
+void printSolution(float [], float [], int);
+void freeEqSolution(EqSolution *);
+float *getODEs1(float [], float);
+float *getODEs2(float [], float);
 
 /** Functions **/
-float dydx1(float x, float y) {return x + y;}
-float dydx2(float x, float y) {return x * x;}
-float dydx3(float x, float y) {return x + y + x * y;}
-float dydx4(float x, float y) {return (x - y) / 2;}
-float dydx5(float x, float y) {return exp(x);}
-
 int main(int argc, char const *argv[]) {
-    float x0 = 0.0, y0 = 1.0, xEnd = 4.0, step = 1.0;
-    float (*dydx)(float, float) = &dydx5;
-
-    runEulers(dydx, x0, y0, xEnd, step);
-    runRungeKutta(dydx, x0, y0, xEnd, step);
+    EqConditions cond = {
+        .x0 = 0.0, 
+        .xEnd = 4.0, 
+        .inits = (float[]){0.0, 0.0, 0.0},
+        .step = 0.5
+    };
+    int stepCount = ceil((cond.xEnd - cond.x0) / cond.step);
+    int numBytes = stepCount * sizeof(float);
+    EqSolution sol1 = {
+        .approx = (float *[]){malloc(numBytes), malloc(numBytes), malloc(numBytes)},
+        .x = malloc(numBytes),
+        .funcCount = 3,
+        .stepCount = stepCount
+    };
+    EqSolution sol2 = {
+        .approx = (float *[]){malloc(numBytes), malloc(numBytes), malloc(numBytes)},
+        .x = malloc(numBytes),
+        .funcCount = 3,
+        .stepCount = stepCount
+    };
     
+    // Run the numerical methods.
+    runEulers(&getODEs2, &cond, &sol1);
+    runRungeKutta(&getODEs2, &cond, &sol2);
+
+    // Print results.
+    printSolution(sol1.approx[0], sol1.x, sol1.stepCount);
+    printSolution(sol2.approx[0], sol2.x, sol2.stepCount);
+
+    // Free heap memory and exit.
+    freeEqSolution(&sol1);
+    freeEqSolution(&sol2);
     exit(EXIT_SUCCESS);
 }
 
-/**
- * @brief 
- * 
- * @param func 
- * @param x0 
- * @param y0 
- * @param xEnd 
- * @param step 
- * @return Solution 
- */
-Solution runEulers(float (*dydx)(float, float), float x0, float y0, float xEnd, float step) {
-    Solution sol;
-    sol.size = ceil((xEnd - x0) / step);
-    float tempY[sol.size];
-    float tempX[sol.size];
-    sol.approx = tempY;
-    sol.x = tempX;
-    sol.approx[0] = y0;
-    sol.x[0] = x0;
-    //printf("Euler's: x0 = %f, y0 = %f, xEnd = %f, step = %f\n", x0, y0, xEnd, step);
 
-    printf("%f\t%f\n", sol.x[0], sol.approx[0]);
-    for (int i = 0; i < sol.size; ++i) {
-        sol.approx[i + 1] = sol.approx[i] + step * (*dydx)(sol.x[i], sol.approx[i]);
-        sol.x[i + 1] = sol.x[i] + step;
-        printf("%f\t%f\n", sol.x[i + 1], sol.approx[i+1]);
+void runEulers(float *(*getODEs)(float [], float), EqConditions *cond, EqSolution *sol) {
+    // Assign initial values for each function and x.
+    float inputs[sol->funcCount];
+    for (int i = 0; i < sol->funcCount; ++i) {
+        sol->approx[i][0] = cond->inits[i];
+        inputs[i] = cond->inits[i];
     }
-    printf("\n");
+    sol->x[0] = cond->x0;
     
-    return sol;
+    // Begin Euler's method.
+    float *slopes;
+    for (int curStep = 0; curStep < sol->stepCount; ++curStep) {
+        slopes = getODEs(inputs, sol->x[curStep]);
+        for (int curFunc = 0; curFunc < sol->funcCount; ++curFunc) {
+            // Calculate the next step for this function.
+            sol->approx[curFunc][curStep + 1] = sol->approx[curFunc][curStep] + cond->step * slopes[curFunc];
+
+            // Update the input for the next step.
+            inputs[curFunc] = sol->approx[curFunc][curStep + 1];
+        }
+        sol->x[curStep + 1] = sol->x[curStep] + cond->step;
+    }
 }
 
-/**
- * @brief 
- * 
- * @param func 
- * @param x0 
- * @param y0 
- * @param xEnd 
- * @param step 
- * @return Solution 
- */
-Solution runRungeKutta(float (*dydx)(float, float), float x0, float y0, float xEnd, float step) {
-    Solution sol;
-    sol.size = ceil((xEnd - x0) / step);
-    float tempY[sol.size];
-    float tempX[sol.size];
-    sol.approx = tempY;
-    sol.x = tempX;
-    sol.approx[0] = y0;
-    sol.x[0] = x0;
-    //printf("Runge-Kutta: x0 = %f, y0 = %f, xEnd = %f, step = %f\n", x0, y0, xEnd, step);
 
-    float k1, k2, k3, k4;
-    printf("%f\t%f\n", sol.x[0], sol.approx[0]);
-    for (int i = 0; i < sol.size; ++i) {
-        k1 = step * (*dydx)(sol.x[i], sol.approx[i]);
-        k2 = step * (*dydx)(sol.x[i] + 0.5 * step, sol.approx[i] + 0.5 * k1);
-        k3 = step * (*dydx)(sol.x[i] + 0.5 * step, sol.approx[i] + 0.5 * k2);
-        k4 = step * (*dydx)(sol.x[i] + step, sol.approx[i] + k3);
+void runRungeKutta(float *(*getODEs)(float [], float), EqConditions *cond, EqSolution *sol) {
+    // Assign initial values for each function and x.
+    float inputs[sol->funcCount];
+    for (int i = 0; i < sol->funcCount; ++i) {
+        sol->approx[i][0] = cond->inits[i];
+        inputs[i] = cond->inits[i];
+    }
+    sol->x[0] = cond->x0;
+    
+    // Begin Runge-Kutta method.
+    float k[4][sol->funcCount], *slopes;
+    for (int curStep = 0; curStep < sol->stepCount; ++curStep) {
+        // Calculate k1 for each function
+        slopes = getODEs(inputs, sol->x[curStep]);
+        for (int curFunc = 0; curFunc < sol->funcCount; ++curFunc) {
+            k[0][curFunc] = cond->step * slopes[curFunc];
+        }
 
-        sol.approx[i + 1] = sol.approx[i] + (k1 + k2 + k2 + k3 + k3 + k4) / 6.0;
+        // Calculate k2 for each function
+        for (int curFunc = 0; curFunc < sol->funcCount; ++curFunc) {
+            inputs[curFunc] = sol->approx[curFunc][curStep] + 0.5 * k[0][curFunc];
+        }
+        slopes = getODEs(inputs, sol->x[curStep] + 0.5 * cond->step);
+        for (int curFunc = 0; curFunc < sol->funcCount; ++curFunc) {
+            k[1][curFunc] = cond->step * slopes[curFunc];
+        }
 
-        sol.x[i + 1] = sol.x[i] + step;
-        printf("%f\t%f\n", sol.x[i + 1], sol.approx[i+1]);
+        // Calculate k3 for each function
+        for (int curFunc = 0; curFunc < sol->funcCount; ++curFunc) {
+            inputs[curFunc] = sol->approx[curFunc][curStep] + 0.5 * k[1][curFunc];
+        }
+        slopes = getODEs(inputs, sol->x[curStep] + 0.5 * cond->step);
+        for (int curFunc = 0; curFunc < sol->funcCount; ++curFunc) {
+            k[2][curFunc] = cond->step * slopes[curFunc];
+        }
+
+        // Calculate k4 for each function
+        for (int curFunc = 0; curFunc < sol->funcCount; ++curFunc) {
+            inputs[curFunc] = sol->approx[curFunc][curStep] + k[2][curFunc];
+        }
+        slopes = getODEs(inputs, sol->x[curStep] + cond->step);
+        for (int curFunc = 0; curFunc < sol->funcCount; ++curFunc) {
+            k[3][curFunc] = cond->step * slopes[curFunc];
+        }
+
+        // Calculate approximation for each function
+        for (int curFunc = 0; curFunc < sol->funcCount; ++curFunc) {
+            sol->approx[curFunc][curStep + 1] = sol->approx[curFunc][curStep] + (k[0][curFunc] + k[1][curFunc] + k[1][curFunc] + k[2][curFunc] + k[2][curFunc] + k[3][curFunc]) / 6.0;
+            
+            // Update the input for the next step.
+            inputs[curFunc] = sol->approx[curFunc][curStep + 1];
+        }
+        
+        sol->x[curStep + 1] = sol->x[curStep] + cond->step;        
+    }
+}
+
+void printSolution(float approx[], float x[], int stepCount) {
+    for (int i = 0; i <= stepCount; i++) {
+        printf("%f\t%f\n", x[i], approx[i]);
     }
     printf("\n");
+}
 
-    return sol;
+void freeEqSolution(EqSolution *sol) {
+    for (int i = 0; i < sol->funcCount; ++i) {
+        free(sol->approx[i]);
+    }
+    free(sol->x);
+}
+
+float *getODEs1(float inputs[], float curX) {
+    static float slopes[1];
+    float x = curX;
+
+    slopes[0] = exp(x);
+
+    return slopes;
+}
+
+float *getODEs2(float inputs[], float curX) {
+    static float slopes[3];
+    float x = inputs[0];    // Voltage
+    float y = inputs[1];    // Spiking
+    float z = inputs[2];    // Bursting
+
+    slopes[0] = y - (x*x*x) + (3*x*x) - z + I;
+    slopes[1] = 1 - (5*x*x) - y;
+    slopes[2] = r * (s * (x - xR) - z);
+
+    return slopes;
 }
